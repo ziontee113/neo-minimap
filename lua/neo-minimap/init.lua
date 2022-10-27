92d313bec3553061eb102c5c4149791a402cfcf1
ziontee113 <ziontee113@gmail.com>
2022-10-28 05:05:06 +0700

    fix(lib): move lib/ into lua/

--- lua/neo-minimap/init.lua
+++ lua/neo-minimap/init.lua
@@ -1,5 +1,16 @@
 local M = {}
+local window_lib = require("neo-minimap.lib.window")
 
 -- TODO:
+
+vim.keymap.set("n", "zi", function()
+	window_lib.open_center_window({
+		open_win_opts = {
+			relative = "cursor",
+			width = 20,
+			height = 10,
+		},
+	})
+end, {})
 
 return M
