a955733caecca1a94eb5bc005f1ed9595a4872d3
ziontee113 <ziontee113@gmail.com>
2022-10-28 10:36:55 +0700

    feat(kh): add helper set_key()

--- lua/neo-minimap/handlers/keymaps.lua
+++ lua/neo-minimap/handlers/keymaps.lua
@@ -1,9 +1,20 @@
 local M = {}
 
+local function set_key(modes, keys, callback, opts)
+	if type(keys) == "string" then
+		keys = { keys }
+	end
+	for _, key in ipairs(keys) do
+		vim.keymap.set(modes, key, callback, opts)
+	end
+end
+
 ---@param state minimap_state
 ---@param lines minimap_line_object[]
 M.handle_keymaps = function(state, lines)
-	-- TODO:
+	set_key("n", { "q", "<Esc>" }, function()
+		vim.api.nvim_win_close(state.minimapWin, true)
+	end, { buffer = state.minimapBuf })
 end
 
 return M
